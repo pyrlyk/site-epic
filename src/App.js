01a9314e366887e0aff508f6e1907b8e31977149
pyrlyk <pyrlykvitaliy@gmail.com>
2020-11-09 15:23:09 +0200

    site epic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './Navbar';
+import Services from './Services';
+import Products from './Products';
+import ContactUs from './ContactUs';
+import SignUp from './SignUp';
+import Consulting from './Consulting';
+import Marketing from './Marketing';
+import Design from './Design';
+import Development from './Development';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Navbar/>
+    <Switch>
+     
+      <Route path = '/services' exact component = {Services}/> 
+      <Route path = '/products' exact component = {Products}/> 
+      <Route path = '/contact-us' exact component = {ContactUs}/> 
+      <Route path = '/sign-up' exact component = {SignUp}/> 
+      <Route path = '/marketing' exact component = {Marketing}/> 
+      <Route path = '/consulting' exact component = {Consulting}/> 
+      <Route path = '/development' exact component = {Development}/>
+      <Route path = '/design' exact component = {Design}/>
+    </Switch>
+    </Router>
+    
   );
 }
 
